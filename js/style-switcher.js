955362da148592c0032a8b14224f43bdd957ed4a
Ricode343 <183218700+Rico-hub-code@users.noreply.github.com>
2026-03-07 08:25:53 +0200

    Refactor style switcher functionality

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -29,7 +29,7 @@ function setActiveStyle(color)
 const dayNight = document.querySelector(".day-night");
 dayNight.addEventListener("click", () =>{
 
-    dayNight.querySelector("i").classList.toggle("fa-sung")
+    dayNight.querySelector("i").classList.toggle("fa-sun")
     dayNight.querySelector("i").classList.toggle("fa-moon")
     document.body.classList.toggle("dark")
 
